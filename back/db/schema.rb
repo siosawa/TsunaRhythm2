26c0357c60f2ffe906ea6d81fdc45526ffa26be4
siosawa <salt.mail0324@gmail.com>
2024-04-28 10:42:00 +0900

    バックエンドの設定ファイルとデータベースのマイグレーションファイルを更新

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_24_004151) do
-  create_table "posts", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2024_04_24_004151) do
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
