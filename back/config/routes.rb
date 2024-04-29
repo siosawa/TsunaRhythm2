da2246eb49a7133bc17522e9af7d76847cdf1dc2
siosawa <salt.mail0324@gmail.com>
2024-04-29 23:44:31 +0900

    add:resources :users

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, format: "json" do
     namespace :v1 do
       resources :posts, only: [:index, :show, :create, :update, :destroy]
+      resources :users
     end
   end
   get "up" => "rails/health#show", as: :rails_health_check
