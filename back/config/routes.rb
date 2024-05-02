3a376473060e0eaf8a9402b67a6c82d7ee79218f
siosawa <salt.mail0324@gmail.com>
2024-05-02 15:07:42 +0900

    モデル作成による自動追記とresources :usersの追加

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get "password_resets/new"
+  get "password_resets/edit"
   get "users/new"
   namespace :api, format: "json" do
     namespace :v1 do
-      resources :posts, only: [:index, :show, :create, :update, :destroy]
+      get "/signup", to: "users#new"
+      get "/login", to: "sessions#new"
+      post "/login", to: "sessions#create"
+      delete "/logout", to: "sessions#destroy"
       resources :users
+      resources :account_activations, only: [:edit]
+      resources :posts, only: [:index, :show, :create, :update, :destroy]
+      # get '/posts', to: 'static_pages#home'
     end
   end
   get "up" => "rails/health#show", as: :rails_health_check
