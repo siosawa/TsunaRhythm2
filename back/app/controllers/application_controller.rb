17d30e811a951dafaa89f0547024bc8a9599bf33
siosawa <salt.mail0324@gmail.com>
2024-05-02 15:17:45 +0900

    add: Railsアプリケーションの初期設定を行うコードで、API専用の設定を有効にし、タイムゾーンを日本に設定し、デフォルトの言語を日本語に設定しています。また、テストを自動生成しないように設定し、CORS（Cross-Origin Resource Sharing）を有効にし、frontからのリソース取得を許可しています。

--- back/app/controllers/application_controller.rb
+++ back/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  # include SessionsHelper
+  # private
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    message = [I18n.t("sessions.flash.danger")]
+    render json: { status: "notLoggedIn", message: message }
+  end
 end
