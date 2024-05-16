f46c895bd9a31dfba40019d2643e6a1734d77d55
siosawa <salt.mail0324@gmail.com>
2024-05-17 02:39:49 +0900

    追加:8000番ポートからの受付とクッキーを含める処理を追加

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,9 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000"
+    origins 'http://localhost:3000', 'http://localhost:8000'
     resource "*",
              headers: :any,
-             methods: %i[get post put patch delete options head]
+             methods: %i[get post put patch delete options head],
+             credentials: true # クッキーを含める場合は true に設定
   end
 end
