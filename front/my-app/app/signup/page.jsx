fe6f705b34cd26e258e2ba694e6618a4c7663d75
siosawa <salt.mail0324@gmail.com>
2024-05-17 02:43:24 +0900

    追加修正:フロントからのアカウント作成機能

--- front/my-app/app/signup/page.jsx
+++ front/my-app/app/signup/page.jsx
@@ -5,10 +5,14 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 const SignUp = () => {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
 
+  const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const confirmPasswordRef = useRef(null);
   const submitButtonRef = useRef(null);
@@ -20,22 +24,65 @@ const SignUp = () => {
     }
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // ここでサインアップロジックを実装します（例：APIにデータを送信）
-    console.log('メールアドレス:', email);
-    console.log('パスワード:', password);
-    console.log('確認用パスワード:', confirmPassword);
+
+    if (password !== confirmPassword) {
+      setError('パスワードが一致しません');
+      return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:3000/api/v1/users', { 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: {
+            name: name,
+            email: email,
+            password: password,
+            password_confirmation: confirmPassword,
+          },
+        }),
+      });
+
+      const responseData = await response.json();
+      if (!response.ok) {
+        setError(responseData.errors.join(', ') || 'ユーザー登録に失敗しました');
+        return;
+      }
+
+      setSuccess('アカウントが作成されました');
+      setName('');
+      setEmail('');
+      setPassword('');
+      setConfirmPassword('');
+      setError('');
+
+    } catch (error) {
+      setError('ユーザー登録に失敗しました');
+    }
   };
 
   return (
     <div className="container shadow-lg p-10 max-w-72 flex flex-col items-center space-y-3 fixed top-20 right-0 lg:right-10 2xl:right-72 my-10 mx-10 rounded-2xl bg-white">
       <h1 className="text-4xl mb-6">新規作成</h1>
       <form onSubmit={handleSubmit} className="w-full space-y-4">
+        {error && <div className="text-red-500">{error}</div>}
+        {success && <div className="text-green-500">{success}</div>}
+        <Input 
+          placeholder="アカウントネーム" 
+          value={name}
+          onChange={(e) => setName(e.target.value)} 
+          onKeyDown={(e) => handleKeyDown(e, emailRef)}
+        />
         <Input 
           placeholder="メールアドレス" 
           value={email}
           onChange={(e) => setEmail(e.target.value)} 
+          ref={emailRef}
           onKeyDown={(e) => handleKeyDown(e, passwordRef)}
         />
         <Input 
