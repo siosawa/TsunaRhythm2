652876c7eafc8a0c9c330d5694a7fca99eb920f4
siosawa <salt.mail0324@gmail.com>
2024-05-17 18:27:18 +0900

    追加:ユーザー一覧ページでAPI通信(ログイン確認なし状態で成功)

--- front/my-app/app/users/page.jsx
+++ front/my-app/app/users/page.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+// 後にdiarys/componentsディレクトリでClientComp.jsを定義してそこで
+// サーバサイドレンダリングが必要な部分だけコンポーネント化してuseClientを実行するのがおそらく望ましい
+"use client"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const page = () => {
+const Users = () => {
+  const [users, setUsers] = useState();
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get("http://localhost:3000/api/v1/users");
+      setUsers(res.data);
+    };
+    getUser();
+    // 空の配列がないと無限ループする
+  }, []);
+
   return (
-    <div>ユーザー一覧ページ</div>
-  )
-}
+    <div>
+      {users?.map((users) => {
+        return (
+          <div key={users.id}>
+            <p>名前: {users.name}</p>
+            <p>メールアドレス: {users.email}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default page
+export default Users;
