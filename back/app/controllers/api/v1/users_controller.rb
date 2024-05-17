382debd2378bb47ae028a2e57aa7695f4ca794d1
siosawa <salt.mail0324@gmail.com>
2024-05-18 01:16:55 +0900

    修正:ユーザーのログイン確認を追加(もとに戻しただけ)

--- back/app/controllers/api/v1/users_controller.rb
+++ back/app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ module Api
       include ActionController::Cookies
       include SessionsHelper
       
-      # before_action :logged_in_user, only: %i[index edit update destroy following followers]
+      before_action :logged_in_user, only: %i[index edit update destroy following followers]
       before_action :correct_user, only: %i[edit update]
       before_action :admin_user, only: :destroy
 
