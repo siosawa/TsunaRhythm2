20573842146b0da71e59e1ab3ead709493e77f69
siosawa <salt.mail0324@gmail.com>
2024-05-24 22:04:34 +0900

    追加:シャドウのカスタム設定を追加

--- front/my-app/tailwind.config.js
+++ front/my-app/tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      boxShadow: {
+        'custom-dark': '0 4px 6px rgba(0, 0, 0, 0.3)',  // カスタムシャドウの追加
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
