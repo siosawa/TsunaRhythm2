4db0670c02d6e1db62484682803bffac27853a29
siosawa <salt.mail0324@gmail.com>
2024-05-28 01:04:03 +0900

    リファ:編集モーダル、ポスト削除機能、ページネイション機能をコンポーネント化して分離

--- front/my-app/app/diarys/components/PostView.jsx
+++ front/my-app/app/diarys/components/PostView.jsx
@@ -3,128 +3,9 @@ import axios from "axios";
 import { format } from "date-fns";
 import ja from "date-fns/locale/ja";
 import Link from "next/link";
-
-const EditPostModal = ({ isOpen, onClose, post, onSave }) => {
-  const [title, setTitle] = useState(post.title);
-  const [content, setContent] = useState(post.content);
-  const [error, setError] = useState("");
-  const contentRef = useRef(null);
-  const titleRef = useRef(null);
-  const submitButtonRef = useRef(null);
-
-  useEffect(() => {
-    if (isOpen && contentRef.current) {
-      contentRef.current.focus();
-    }
-
-    const handleKeyDown = (event) => {
-      if ((event.metaKey || event.ctrlKey) && event.key === "Enter") {
-        event.preventDefault();
-        if (submitButtonRef.current) {
-          submitButtonRef.current.click();
-        }
-      }
-    };
-
-    if (isOpen) {
-      window.addEventListener("keydown", handleKeyDown);
-    } else {
-      window.removeEventListener("keydown", handleKeyDown);
-    }
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [isOpen]);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (title.length > 56) {
-      setError("タイトル分は56文字までです");
-      return;
-    } else if (content.length > 2050) {
-      setError("投稿は2050文字までです");
-      return;
-    }
-
-    const finalTitle = title || content.slice(0, 40);
-
-    try {
-      await onSave(post.id, finalTitle, content);
-      onClose();
-    } catch (error) {
-      console.error("ポストの編集に失敗しました:", error);
-      setError("ポストの編集に失敗しました。");
-    }
-  };
-
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-    if (e.target.value.length > 56) {
-      setError("タイトル文は56文字までです");
-    } else {
-      setError("");
-    }
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
-    if (e.target.value.length > 2050) {
-      setError("投稿は2050文字までです");
-    } else {
-      setError("");
-    }
-  };
-
-  if (!isOpen) return null;
-
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-11/12 md:w-2/3 lg:w-1/2">
-        {error && <div className="text-red-500">{error}</div>}
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <input
-              placeholder="日記タイトル"
-              id="title"
-              value={title}
-              onChange={handleTitleChange}
-              ref={titleRef}
-              className="w-full px-3 py-6 text-3xl rounded transition-all outline-none"
-            />
-          </div>
-          <div className="mb-4">
-            <textarea
-              placeholder="ご自由にお書きください"
-              id="content"
-              value={content}
-              onChange={handleContentChange}
-              ref={contentRef}
-              className="w-full px-3 h-96 rounded transition-all outline-none"
-            />
-          </div>
-          <div className="flex items-center justify-end">
-            <button
-              type="button"
-              onClick={onClose}
-              className="bg-gray-500 text-white px-4 py-2 rounded mr-2"
-            >
-              キャンセル
-            </button>
-            <button
-              type="submit"
-              ref={submitButtonRef}
-              className="bg-blue-500 text-white px-4 py-2 rounded"
-            >
-              保存
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-};
+import EditPostModal from "@/app/diarys/components/EditPostModal";
+import PostDelete from "@/app/diarys/components/PostDelete";
+import PostPagination from "@/app/diarys/components/PostPagination";
 
 const PostView = ({ reload }) => {
   const [posts, setPosts] = useState([]);
@@ -152,15 +33,8 @@ const PostView = ({ reload }) => {
     fetchPosts();
   }, [reload, currentPage]);
 
-  const handleDelete = async (postId) => {
-    try {
-      await axios.delete(`http://localhost:3000/api/v1/posts/${postId}`, {
-        withCredentials: true,
-      });
-      setPosts(posts.filter((post) => post.id !== postId));
-    } catch (error) {
-      console.error("ポストの削除に失敗しました:", error);
-    }
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
   };
 
   const handleEditClick = (post) => {
@@ -189,41 +63,10 @@ const PostView = ({ reload }) => {
         )
       );
       setEditingPost(null);
+      setIsModalOpen(false); // Save後にモーダルを閉じる
     } catch (error) {
       console.error("ポストの編集に失敗しました:", error);
     }
-  };
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
-
-  const renderPageNumbers = () => {
-    const pages = [];
-    const maxPagesToShow = 5; // 表示するページ数を制限
-    const halfWindow = Math.floor(maxPagesToShow / 2);
-    let startPage = Math.max(currentPage - halfWindow, 1);
-    let endPage = Math.min(currentPage + halfWindow, totalPages);
-
-    if (startPage > 1) {
-      pages.push(1);
-      if (startPage > 2) {
-        pages.push("...");
-      }
-    }
-
-    for (let i = startPage; i <= endPage; i++) {
-      pages.push(i);
-    }
-
-    if (endPage < totalPages) {
-      if (endPage < totalPages - 1) {
-        pages.push("...");
-      }
-      pages.push(totalPages);
-    }
-
-    return pages;
   };
 
   return (
@@ -265,12 +108,11 @@ const PostView = ({ reload }) => {
                     >
                       編集
                     </button>
-                    <button
-                      onClick={() => handleDelete(post.id)}
-                      className="px-4 py-2 bg-red-500 text-white rounded-md"
-                    >
-                      削除
-                    </button>
+                    <PostDelete
+                      postId={post.id}
+                      posts={posts}
+                      setPosts={setPosts}
+                    />
                   </>
                 )}
               </div>
@@ -278,22 +120,11 @@ const PostView = ({ reload }) => {
           </div>
         );
       })}
-      <div className="flex justify-center space-x-4 mt-4">
-        {renderPageNumbers().map((page, index) => (
-          <button
-            key={index}
-            onClick={() => page !== "..." && handlePageChange(page)}
-            disabled={page === currentPage}
-            className={`px-4 py-2 rounded ${
-              page === currentPage
-                ? "bg-blue-500 text-white"
-                : "bg-gray-300 text-gray-700"
-            }`}
-          >
-            {page}
-          </button>
-        ))}
-      </div>
+      <PostPagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
       {editingPost && (
         <EditPostModal
           isOpen={isModalOpen}
