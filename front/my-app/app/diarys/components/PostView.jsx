16dacffa8a58157c72f1233cc688608292a3dd10
siosawa <salt.mail0324@gmail.com>
2024-05-29 06:42:13 +0900

    削除:不要な記述を削除

--- front/my-app/app/diarys/components/PostView.jsx
+++ front/my-app/app/diarys/components/PostView.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { format } from "date-fns";
 import ja from "date-fns/locale/ja";
