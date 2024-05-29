82611b1654297b91462253ca43238d087fd1bcf4
siosawa <salt.mail0324@gmail.com>
2024-05-30 07:25:10 +0900

    追加:RouteHandlerを使用したデータ取得(失敗)

---
+++ front/my-app/app/api/users/route.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server"
+
+// export const GET = () => {
+//     return NextResponse.json({ hello: "hello" })
+// }
+
+export async function GET() {
+    const res = await fetch('http://localhost:3000/api/v1/users')
+    const data = await res.json()
+    return NextResponse.json({ data })
+}
