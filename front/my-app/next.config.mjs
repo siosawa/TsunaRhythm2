f6eb21934c8a038088d7a3a56eee9ea9904309bc
siosawa <salt.mail0324@gmail.com>
2024-05-31 11:51:57 +0900

    追加:Reactの厳密モードを有効にし、特定のドメインとパスからの外部画像を許可する設定もより厳密にした

--- front/my-app/next.config.mjs
+++ front/my-app/next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['localhost'], 
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
