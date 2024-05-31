4bd8813c6c218b3f981863126b0b91b652c65710
siosawa <salt.mail0324@gmail.com>
2024-06-01 05:02:02 +0900

    削除:エラーハンドリングを削除

--- front/my-app/app/api/users/route.js
+++ front/my-app/app/api/users/route.js
@@ -1,26 +1,11 @@
 import { NextResponse } from "next/server"
-
 // export const GET = () => {
 //     return NextResponse.json({ hello: "hello" })
 // }
-
 export async function GET() {
-    try {
-        const res = await fetch('http://localhost:3000/api/v1/users');
-
-        if (!res.ok) {
-            return NextResponse.json({ error: 'Failed to fetch users data' }, { status: res.status });
-        }
-
-        const data = await res.json();
-
-        if (!data) {
-            return NextResponse.json({ error: 'No data returned from the API' }, { status: 500 });
-        }
-
-        return NextResponse.json({ data });
-    } catch (error) {
-        console.error('Error fetching users data:', error);
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-    }
+    const res = await fetch('http://localhost:3000/api/v1/users');
+    const data = await res.json();
+    return NextResponse.json({ data });
 }
+
+
