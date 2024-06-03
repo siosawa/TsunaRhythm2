2ea19d11e0432daaa7e84d90d296bdb528281fa5
siosawa <salt.mail0324@gmail.com>
2024-06-03 14:16:20 +0900

    リファ: ボタンの名前を変更、表示するデータを修正、コンソールで取れてきているデータを確認

--- front/my-app/app/users/components/UserCard.jsx
+++ front/my-app/app/users/components/UserCard.jsx
@@ -1,4 +1,7 @@
-import FollowButton from "@/app/users/components/FollowButton";
+import { useEffect } from "react";
+import FollowUnfollowButton from "@/app/users/components/FollowUnfollowButton";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const UserCard = ({
   user,
@@ -7,21 +10,40 @@ const UserCard = ({
   setFollowStates,
   setFollowings,
 }) => {
+  useEffect(() => {
+    console.log("User data:", user);
+    console.log("Followings:", followings);
+    console.log("FollowStates:", followStates);
+  }, [user, followings, followStates]);
+
   return (
     <div
       key={user.id}
       className="bg-white shadow-md rounded-lg p-6 mb-4 flex justify-between items-center"
     >
       <div>
-        <p className="text-lg font-semibold mb-2">名前: {user.name}</p>
-        <p className="text-gray-600 mb-1">メールアドレス: {user.email}</p>
-        <p className="text-gray-600 mb-1">ID: {user.id}</p>
+        <Link href={`/users/${user.id}`}>
+          <Button className="text-lg font-semibold mb-2 hover:underline bg-transparent text-black hover:bg-transparent">
+            名前: {user.name}
+          </Button>
+        </Link>
+        <Link href={`/users/${user.id}/following`}>
+          <Button className="text-lg font-semibold mb-2 hover:underline bg-transparent text-black hover:bg-transparent">
+            フォロー数: {user.following_count}
+          </Button>
+        </Link>
+        <Link href={`/users/${user.id}/followers`}>
+          <Button className="text-lg font-semibold mb-2 hover:underline bg-transparent text-black hover:bg-transparent">
+            フォロワー数: {user.followers_count}
+          </Button>
+        </Link>
         <p className="text-gray-600 mb-1">ポスト数: {user.posts_count}</p>
+        <p className="text-gray-600 mb-1">ワーク: {user.work}</p>
         <p className="text-gray-600">
           アカウント作成日: {new Date(user.created_at).toLocaleDateString()}
         </p>
       </div>
-      <FollowButton
+      <FollowUnfollowButton
         userId={user.id}
         isFollowing={followings.has(user.id)}
         followStates={followStates}
