8141e325a9e7d0063f88df92e5458216d822aaaa
siosawa <salt.mail0324@gmail.com>
2024-06-03 14:24:19 +0900

    リファ:/followersを/usersと同じように閲覧

---
+++ front/my-app/app/users/[userId]/followers/page.jsx
@@ -0,0 +1,68 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import UserCard from "@/app/users/components/UserCard";
+import FetchFollowers from "@/app/users/[userId]/followers/components/FetchFollowers";
+import PaginationHandler from "@/app/users/components/PagenationHandler";
+
+// メインコンポーネント
+const UsersList = () => {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentUserId, setCurrentUserId] = useState(null);
+  const [followings, setFollowings] = useState(new Set());
+  const [followStates, setFollowStates] = useState({});
+
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/v1/current_user",
+          {
+            withCredentials: true,
+          }
+        );
+        const userData = response.data;
+        setCurrentUserId(userData.id);
+      } catch (error) {
+        console.error("現在のユーザー情報の取得に失敗しました:", error);
+      }
+    };
+
+    fetchCurrentUser();
+  }, []);
+
+  return (
+    <div className="p-4">
+      <FetchFollowers
+        currentPage={currentPage}
+        currentUserId={currentUserId}
+        setUsers={setUsers}
+        setTotalPages={setTotalPages}
+        setFollowings={setFollowings}
+        setFollowStates={setFollowStates}
+        setError={setError}
+      />
+      {error && <div>{error}</div>}
+      {users?.map((user) => (
+        <UserCard
+          key={user.id}
+          user={user}
+          followings={followings}
+          followStates={followStates}
+          setFollowStates={setFollowStates}
+          setFollowings={setFollowings}
+        />
+      ))}
+      <PaginationHandler
+        currentPage={currentPage}
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+      />
+    </div>
+  );
+};
+
+export default UsersList;
