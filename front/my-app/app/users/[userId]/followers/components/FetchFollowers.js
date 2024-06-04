4a516472aebf558f2ff4986a2c14bf10a69adefe
siosawa <salt.mail0324@gmail.com>
2024-06-04 10:50:26 +0900

    修正:FetchFollowingの表記と統一「fetchFollowingId」

--- front/my-app/app/users/[userId]/followers/components/FetchFollowers.js
+++ front/my-app/app/users/[userId]/followers/components/FetchFollowers.js
@@ -1,94 +1,3 @@
-// import { useEffect } from "react";
-// import axios from "axios";
-// import { useParams } from 'next/navigation';
-
-// // ユーザー情報取得コンポーネント
-// const FetchFollowers = ({
-//   currentPage,
-//   currentUserId,
-//   setUsers,
-//   setTotalPages,
-//   setFollowings,
-//   setFollowStates,
-//   setError
-// }) => {
-//   const { userId } = useParams();
-
-//   useEffect(() => {
-//     const fetchFollowers = async () => {
-//       try {
-//         const res = await axios.get(
-//           `http://localhost:3000/api/v1/users/${userId}/followers?page=${currentPage}`,
-//           {
-//             withCredentials: true,
-//           }
-//         );
-
-//         if (res.data && Array.isArray(res.data.users)) {
-//           const filteredUsers = res.data.users.map(user => ({
-//             id: user.id,
-//             name: user.name,
-//             created_at: user.created_at,
-//             updated_at: user.updated_at,
-//             work: user.work,
-//             profile_text: user.profile_text,
-//             avatar: user.avatar,
-//             relationship_id: user.relationship_id,
-//             posts_count: user.posts_count,
-//             followers_count: user.followers_count,
-//             following_count: user.following_count,
-//           }));
-//           setUsers(filteredUsers);
-//           setTotalPages(res.data.total_pages);
-//         } else {
-//           console.error("APIレスポンスは正しくありません:", res.data);
-//           setError("データの取得に失敗しました。");
-//         }
-//       } catch (err) {
-//         console.error("API呼び出しに失敗しました:", err);
-//         setError("データの取得に失敗しました。");
-//       }
-//     };
-
-//     const fetchFollowing = async () => {
-//       if (currentUserId) {
-//         try {
-//           const response = await axios.get(
-//             `http://localhost:3000/api/v1/users/${currentUserId}/following`,
-//             {
-//               withCredentials: true,
-//             }
-//           );
-//           const data = response.data;
-
-//           const followingSet = new Set(data.users.map((user) => user.id));
-//           const followingStates = {};
-//           data.users.forEach((user) => {
-//             followingStates[user.id] = true;
-//             followingStates[`relationship_${user.id}`] = user.relationship_id;
-//           });
-
-//           setFollowings(followingSet);
-//           setFollowStates(followingStates);
-//         } catch (error) {
-//           console.error("フォロー中のユーザーの取得に失敗しました:", error);
-//         }
-//       }
-//     };
-
-//     if (userId) {
-//       fetchFollowers();
-//     }
-//     if (currentUserId) {
-//       fetchFollowing();
-//     }
-//   }, [currentPage, currentUserId, userId, setUsers, setTotalPages, setFollowings, setFollowStates, setError]);
-
-//   return null; // このコンポーネントは何もレンダリングしないため、nullを返します。
-// };
-
-// export default FetchFollowers;
-
 import { useEffect } from "react";
 import axios from "axios";
 import { useParams } from 'next/navigation';
@@ -104,6 +13,7 @@ const FetchFollowers = ({
   setError
 }) => {
   const { userId } = useParams();
+  console.log( userId )
 
   useEffect(() => {
     const fetchFollowers = async () => {
@@ -128,7 +38,7 @@ const FetchFollowers = ({
       }
     };
 
-    const fetchFollowing = async () => {
+    const fetchFollowingId = async () => {
       if (currentUserId) {
         try {
           const response = await axios.get(
@@ -158,11 +68,11 @@ const FetchFollowers = ({
       fetchFollowers();
     }
     if (currentUserId) {
-      fetchFollowing();
+      fetchFollowingId();
     }
   }, [currentPage, currentUserId, userId]);
 
-  return null; // このコンポーネントは何もレンダリングしないため、nullを返します。
+  return null; 
 };
 
 export default FetchFollowers;
