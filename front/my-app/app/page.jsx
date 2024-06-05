3c69940ac0d726c33b6aa98f50bd25fb8bf5cfda
siosawa <salt.mail0324@gmail.com>
2024-06-05 14:13:28 +0900

    追加:背景を追加

--- front/my-app/app/page.jsx
+++ front/my-app/app/page.jsx
@@ -1,8 +1,9 @@
 import Login from "@/components/Login";
+
 export default function Home() {
   return (
-    <>
+    <div className="bg-[url('./data/background_image.png')] bg-cover bg-center min-h-screen flex items-center justify-center">
       <Login />
-    </>
+    </div>
   );
 }
