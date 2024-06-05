9a2210e098447b90d13692d0a8b40360803f3f53
siosawa <salt.mail0324@gmail.com>
2024-06-05 14:59:52 +0900

    修正:背景を動く雲の動画に変更

--- front/my-app/app/page.jsx
+++ front/my-app/app/page.jsx
@@ -2,8 +2,20 @@ import Login from "@/components/Login";
 
 export default function Home() {
   return (
-    <div className="bg-[url('./data/background_image.png')] bg-cover bg-center min-h-screen flex items-center justify-center">
-      <Login />
+    <div className="relative min-h-screen flex items-center justify-center">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        src="/background_movie.MP4"
+        autoPlay
+        loop
+        muted
+      />
+
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 z-10"></div>
+
+      <div className="relative z-20">
+        <Login />
+      </div>
     </div>
   );
 }
