7f92b1dff57835945bd572bee265fe090f3b41cc
siosawa <salt.mail0324@gmail.com>
2024-06-05 15:56:58 +0900

    修正:全てのページの背景で雲動画が流れるように再設定

--- front/my-app/app/rooms/page.jsx
+++ front/my-app/app/rooms/page.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 
 const index = () => {
-  return (
-    <div className="relative min-h-screen flex items-center justify-center">
-      <video
-        className="absolute top-0 left-0 w-full h-full object-cover z-0"
-        src="/background_movie.MP4"
-        autoPlay
-        loop
-        muted
-      />
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 z-10"></div>
-      {/* ここにコードを追加 */}
-    </div>
-  );
+  return <div>ルーム一覧・作成ページ</div>;
 };
 
 export default index;
