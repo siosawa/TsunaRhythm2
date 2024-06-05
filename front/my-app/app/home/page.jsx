e7ba3f95ed62d103cbc06b858558dd090d02cc9f
siosawa <salt.mail0324@gmail.com>
2024-06-05 15:03:20 +0900

    追加:動く壁紙をホームとroomsに追加

--- front/my-app/app/home/page.jsx
+++ front/my-app/app/home/page.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const index = () => {
+  return (
+    <div className="relative min-h-screen flex items-center justify-center">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        src="/background_movie.MP4"
+        autoPlay
+        loop
+        muted
+      />
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 z-10"></div>
+      {/* ここにコードを追加 */}
+    </div>
+  );
 };
 
-export default page;
+export default index;
