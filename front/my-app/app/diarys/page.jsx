b4d6c1364df977523ba0917502fd7bc67831766c
siosawa <salt.mail0324@gmail.com>
2024-06-05 15:15:17 +0900

    修正:背景に雲の動画背景が来るように変更

--- front/my-app/app/diarys/page.jsx
+++ front/my-app/app/diarys/page.jsx
@@ -11,10 +11,20 @@ const Post = () => {
   };
 
   return (
-    <>
-      <PostInput onPostSuccess={handleReload} />
-      <PostView reload={reload} />
-    </>
+    <div className="relative min-h-screen">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        src="/background_movie.MP4"
+        autoPlay
+        loop
+        muted
+      />
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 z-10"></div>{" "}
+      <div className="relative z-20 flex flex-col items-center justify-center min-h-screen">
+        <PostInput onPostSuccess={handleReload} />
+        <PostView reload={reload} />
+      </div>
+    </div>
   );
 };
 
