ca4244c8e2b2b65fa97d813108326344358b49cf
siosawa <salt.mail0324@gmail.com>
2024-06-05 09:27:50 +0900

    リファ: FetchPostsコンポーネントを分離してインポート・適用

--- front/my-app/app/diarys/components/PostView.jsx
+++ front/my-app/app/diarys/components/PostView.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { format } from "date-fns";
 import ja from "date-fns/locale/ja";
@@ -7,6 +7,7 @@ import EditPostModal from "@/app/diarys/components/EditPostModal";
 import PostDelete from "@/app/diarys/components/PostDelete";
 import PostPagination from "@/app/diarys/components/PostPagination";
 import { Button } from "@/components/ui/button";
+import FetchPosts from "./FetchPosts";
 
 const PostView = ({ reload }) => {
   const [posts, setPosts] = useState([]);
@@ -14,25 +15,6 @@ const PostView = ({ reload }) => {
   const [totalPages, setTotalPages] = useState(1);
   const [editingPost, setEditingPost] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:3000/api/v1/posts?page=${currentPage}`,
-          {
-            withCredentials: true,
-          }
-        );
-
-        setPosts(response.data.posts);
-        setTotalPages(response.data.total_pages);
-      } catch (error) {
-        console.error("データの取得に失敗しました:", error);
-      }
-    };
-    fetchPosts();
-  }, [reload, currentPage]);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
@@ -64,7 +46,7 @@ const PostView = ({ reload }) => {
         )
       );
       setEditingPost(null);
-      setIsModalOpen(false); // Save後にモーダルを閉じる
+      setIsModalOpen(false);
     } catch (error) {
       console.error("ポストの編集に失敗しました:", error);
     }
@@ -82,10 +64,27 @@ const PostView = ({ reload }) => {
           <div key={post.id} className="border-b border-gray-200 py-4">
             <div className="flex items-center mb-2">
               <div className="w-10 h-10 bg-gray-200 rounded-full mr-4 flex-shrink-0"></div>
-              <div className="flex flex-col">
-                <div className="flex items-center space-x-2">
-                  <p className="text-lg font-semibold">{post.user.name}</p>
-                  <p className="text-sm text-gray-500">{formattedDate}</p>
+              <div className="flex flex-col flex-1">
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold mr-2">{post.user.name}</p>
+                  <div className="flex items-center space-x-2">
+                    <p className="text-sm text-gray-500">{formattedDate}</p>
+                    {post.user_id === post.current_user_id && (
+                      <>
+                        <Button
+                          className="text-black bg-transparent hover:bg-gray-100"
+                          onClick={() => handleEditClick(post)}
+                        >
+                          編集
+                        </Button>
+                        <PostDelete
+                          postId={post.id}
+                          posts={posts}
+                          setPosts={setPosts}
+                        />
+                      </>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
@@ -100,27 +99,16 @@ const PostView = ({ reload }) => {
                   ? `${post.content.slice(0, 430)}...`
                   : post.content}
               </div>
-              <div className="space-x-2">
-                {post.user_id === post.current_user_id && (
-                  <>
-                    <Button
-                      className="text-black bg-transparent hover:bg-gray-100"
-                      onClick={() => handleEditClick(post)}
-                    >
-                      編集
-                    </Button>
-                    <PostDelete
-                      postId={post.id}
-                      posts={posts}
-                      setPosts={setPosts}
-                    />
-                  </>
-                )}
-              </div>
             </div>
           </div>
         );
       })}
+      <FetchPosts
+        currentPage={currentPage}
+        setPosts={setPosts}
+        setTotalPages={setTotalPages}
+        reload={reload}
+      />
       <PostPagination
         currentPage={currentPage}
         totalPages={totalPages}
