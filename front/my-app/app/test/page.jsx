386c091d36cb9c300654fecc9fb08dfc55e2cc99
siosawa <salt.mail0324@gmail.com>
2024-06-06 16:13:48 +0900

    test:shadcn/uiのボタンテスト

--- front/my-app/app/test/page.jsx
+++ front/my-app/app/test/page.jsx
@@ -1,59 +1,14 @@
-// app/test/page.jsx
-"use client";
+import { Button } from "@/components/ui/button";
 
-import { useEffect, useState } from "react";
-
-const Home = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "http://localhost:3000/api/v1/users/59/following?page=1"
-        );
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const jsonData = await response.json();
-        console.log(jsonData);
-
-        // Access the 'users' property from the response data
-        if (jsonData.users && Array.isArray(jsonData.users)) {
-          setData(jsonData.users);
-        } else {
-          throw new Error("Data format is not as expected");
-        }
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
+export default function MyComponent() {
   return (
     <div>
-      <h1>Following List</h1>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+      <Button variant="primary">Primary Button</Button>
+      <Button variant="secondary">Secondary Button</Button>
+      <Button variant="destructive">Destructive Button</Button>
+      <Button variant="outline">Outline Button</Button>
+      <Button variant="ghost">Ghost Button</Button>
+      <Button variant="link">Link Button</Button>
     </div>
   );
-};
-
-export default Home;
+}
