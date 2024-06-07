2710be089f5b6bcc11337561d96f0dedc82b1bc9
siosawa <salt.mail0324@gmail.com>
2024-06-07 11:58:19 +0900

    追加: FactoryBotのメソッドをインクルードする設定を追加

--- back/spec/rails_helper.rb
+++ back/spec/rails_helper.rb
@@ -1,3 +1,4 @@
+# spec/rails_helper.rb
 require 'spec_helper'
 require 'faker'
 ENV['RAILS_ENV'] ||= 'test'
@@ -15,4 +16,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  # FactoryBotのメソッドをインクルードする設定
+  config.include FactoryBot::Syntax::Methods
 end
