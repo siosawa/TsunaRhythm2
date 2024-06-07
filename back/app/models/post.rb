2b7a3e6a8282d89b99a88abab5e403d66ec1ef66
siosawa <salt.mail0324@gmail.com>
2024-06-08 00:02:07 +0900

    削除:不要なバリデーションを削除(rubocop -A)

--- back/app/models/post.rb
+++ back/app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   # user_idのバリデーションコメントアウトを解除
-  validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 56 }
   validates :content, presence: true, length: { maximum: 2050 }
 end
