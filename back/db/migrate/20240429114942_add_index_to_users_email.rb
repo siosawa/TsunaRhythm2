e7cf88c7f60138455c7224483f293283a9b39512
siosawa <salt.mail0324@gmail.com>
2024-06-09 13:50:10 +0900

    fix:githubActionsテストのmigrateの際にindexの重複エラーが出たため内容をemailにuniqueがついていなかったら付与するように修正

--- back/db/migrate/20240429114942_add_index_to_users_email.rb
+++ back/db/migrate/20240429114942_add_index_to_users_email.rb
@@ -1,5 +1,7 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[7.0]
   def change
-    add_index :users, :email, unique: true
+    unless index_exists?(:users, :email, unique: true)
+      add_index :users, :email, unique: true
+    end
   end
 end
