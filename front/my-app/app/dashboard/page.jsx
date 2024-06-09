d0d7eacff26c05490276668a96ae541a7fd40af8
siosawa <salt.mail0324@gmail.com>
2024-06-10 02:07:23 +0900

    追加:カレンダーコンポーネントを表示

--- front/my-app/app/dashboard/page.jsx
+++ front/my-app/app/dashboard/page.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-
-const dashboard = () => {
-  return <div>dashboard</div>;
+import { Calendar } from "@/components/ui/calendar";
+const Dashboard = () => {
+  return (
+    <>
+      <div>dashboard?</div>
+      <Calendar className="pl-9 mx-80 my-10 bg-white rounded-3xl shadow-custom-dark" />
+    </>
+  );
 };
 
-export default dashboard;
+export default Dashboard;
+
+{
+  /* <Logo />
+<MonthHWAT />
+<RandomWord />
+<CaseHW />
+<WorkHW />
+<GraphHWA />
+<GraphHWT />
+<Calendar />
+<EditTable />  */
+}
