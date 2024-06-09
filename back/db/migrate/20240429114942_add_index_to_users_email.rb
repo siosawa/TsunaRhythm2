34d9265ae1dd888f818fa7de885f35311e99184f
siosawa <salt.mail0324@gmail.com>
2024-06-09 14:53:17 +0900

    修正:rubocopによるUserモデルに対して一意性のバリデーションが設定されているが、そのデータベース列にユニークインデックスが設定されていない警告を解消

--- back/db/migrate/20240429114942_add_index_to_users_email.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexToUsersEmail < ActiveRecord::Migration[7.0]
-  def change
-    unless index_exists?(:users, :email, unique: true)
-      add_index :users, :email, unique: true
-    end
-  end
-end
--- back/db/schema.rb
+++ back/db/schema.rb
@@ -35,13 +35,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_30_211327) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "work"
-    t.text "profile_text"
-    t.string "avatar"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
