4f3ae4b33e2ce43c12b6916da3f878fe3788a591
siosawa <salt.mail0324@gmail.com>
2024-06-09 20:56:42 +0900

    追加:  add_foreign_key "posts", "users"

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -73,4 +73,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_30_211327) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
