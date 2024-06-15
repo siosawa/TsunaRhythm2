2832b9a0e9af33252722b1730faee48f1eb06e44
siosawa <salt.mail0324@gmail.com>
2024-06-16 03:57:34 +0900

    fix:日毎の自給平均を正確に測定するように修正

--- front/my-app/app/dashboard/components/GraphHourlyRate.jsx
+++ front/my-app/app/dashboard/components/GraphHourlyRate.jsx
@@ -41,61 +41,82 @@ const GraphHourlyRate = () => {
   });
 
   const [monthIndex, setMonthIndex] = useState(currentMonth); // 初期値を現在の月に設定
+  const [error, setError] = useState(null);
 
-  const fetchData = (month) => {
-    Promise.all([
-      axios.get("http://localhost:3001/projects"),
-      axios.get("http://localhost:3001/records"),
-    ])
-      .then(([projectsResponse, recordsResponse]) => {
-        const projects = projectsResponse.data || [];
-        const records = recordsResponse.data || [];
-        const hourlyRatePerDay = {};
-        const selectedMonthRecords = records.filter(
-          (record) => new Date(record.date).getMonth() === month
+  const fetchData = async (month) => {
+    try {
+      const [projectsResponse, recordsResponse] = await Promise.all([
+        axios.get("http://localhost:3001/projects"),
+        axios.get("http://localhost:3001/records"),
+      ]);
+      const projects = projectsResponse.data || [];
+      const records = recordsResponse.data || [];
+      const hourlyRatePerDay = {};
+      const earningsPerDay = {}; // 日毎の稼いだ金額
+      const workMinutesPerDay = {}; // 日毎の作業時間
+      const selectedMonthRecords = records.filter(
+        (record) => new Date(record.date).getMonth() === month
+      );
+
+      const year = new Date().getFullYear();
+
+      projects.forEach((project) => {
+        const projectRecords = selectedMonthRecords.filter(
+          (record) => record.project_id === project.id
+        );
+        const totalWorkMinutes = projectRecords.reduce(
+          (total, record) => total + record.minutes,
+          0
         );
 
-        selectedMonthRecords.forEach((record) => {
-          const project = projects.find((p) => p.id === record.project_id);
-          if (project) {
-            const date = new Date(record.date).toLocaleDateString();
-            if (!hourlyRatePerDay[date]) {
-              hourlyRatePerDay[date] = 0;
-            }
-            const hourlyRate =
-              (project.unitPrice * project.quantity) / (record.minutes / 60);
-            hourlyRatePerDay[date] += hourlyRate;
+        const averageHourlyWage =
+          (project.unitPrice * project.quantity) / (totalWorkMinutes / 60);
+
+        projectRecords.forEach((record) => {
+          const date = new Date(record.date).toLocaleDateString();
+          if (!earningsPerDay[date]) {
+            earningsPerDay[date] = 0;
+            workMinutesPerDay[date] = 0;
           }
-        });
 
-        const year = new Date().getFullYear();
+          // recordsの作業分数と時給を分給に直したものをかけて稼いだ金額を出力
+          const dailyEarnings = record.minutes * (averageHourlyWage / 60);
 
-        setChartData({
-          labels: Object.keys(hourlyRatePerDay),
-          datasets: [
-            {
-              label: `${year}年${month + 1}月の日毎の時給`,
-              data: Object.values(hourlyRatePerDay),
-              backgroundColor: "rgba(75, 192, 192, 0.2)",
-              borderColor: "rgba(75, 192, 192, 1)",
-              borderWidth: 1,
-              tension: 0.4, // ラインのテンションを設定して波線にする
-            },
-          ],
+          // 日毎に稼いだ金額を加算していくことで日毎の稼いだ金額合計を出力
+          earningsPerDay[date] += dailyEarnings;
+
+          // 日毎の作業時間を加算していくことで日毎の作業時間合計を出力
+          workMinutesPerDay[date] += record.minutes;
         });
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
       });
+
+      Object.keys(earningsPerDay).forEach((date) => {
+        hourlyRatePerDay[date] =
+          earningsPerDay[date] / (workMinutesPerDay[date] / 60);
+      });
+
+      setChartData({
+        labels: Object.keys(hourlyRatePerDay),
+        datasets: [
+          {
+            label: `${year}年${month + 1}月の日毎の時給`,
+            data: Object.values(hourlyRatePerDay),
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
+            tension: 0.4, // ラインのテンションを設定して波線にする
+          },
+        ],
+      });
+    } catch (error) {
+      setError("データの取得に失敗しました。");
+      console.error("Error fetching data:", error);
+    }
   };
 
   useEffect(() => {
     fetchData(monthIndex);
   }, [monthIndex]);
-
-  useEffect(() => {
-    fetchData(currentMonth); // 初回ロード時に現在の月のデータを取得
-  }, []);
 
   const handlePreviousMonth = () => {
     setMonthIndex((prevMonthIndex) =>
@@ -110,7 +131,8 @@ const GraphHourlyRate = () => {
   };
 
   return (
-    <div className="w-96 h-58 p-4 mx-7 bg-white rounded-3xl custom_shadow_dark relative">
+    <div className="w-96 h-60 p-4 mx-7 bg-white rounded-3xl shadow-md relative">
+      {error && <p className="text-red-500">{error}</p>}
       <Line
         data={chartData}
         options={{
