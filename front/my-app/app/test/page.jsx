241a03b8dd87f29d173f91369e226b98b724b60d
siosawa <salt.mail0324@gmail.com>
2024-06-15 16:30:44 +0900

    chore

--- front/my-app/app/test/page.jsx
+++ front/my-app/app/test/page.jsx
@@ -1,14 +1,6 @@
-import { Button } from "@/components/ui/button";
+import "../api/dashboards";
+const page = () => {
+  return <div>page</div>;
+};
 
-export default function MyComponent() {
-  return (
-    <div>
-      <Button variant="primary">Primary Button</Button>
-      <Button variant="secondary">Secondary Button</Button>
-      <Button variant="destructive">Destructive Button</Button>
-      <Button variant="outline">Outline Button</Button>
-      <Button variant="ghost">Ghost Button</Button>
-      <Button variant="link">Link Button</Button>
-    </div>
-  );
-}
+export default page;
