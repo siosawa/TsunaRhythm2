5213b3a53b0f9d54c93c2e0fcafa16c0deb26dc7
siosawa <salt.mail0324@gmail.com>
2024-06-17 07:15:23 +0900

    chore:ユーザーアバターの画像の取得テストに成功

--- front/my-app/app/test/page.jsx
+++ front/my-app/app/test/page.jsx
@@ -1,16 +1,14 @@
-import Image from "next/image";
-
 const AvatarComponent = () => {
   const avatarUrl = "http://localhost:3000/uploads/user/avatar/2/IMG_1515.jpg";
 
   return (
     <div>
-      <p className="text-3xl mt-16">アバターを表示</p>
-      <Image
+      <img
         src={avatarUrl}
         alt="User Avatar"
-        width={500} // 必要に応じてサイズを指定
-        height={150} // 必要に応じてサイズを指定
+        width={100} // 必要に応じてサイズを指定
+        height={100} // 必要に応じてサイズを指定
+        style={{ borderRadius: "50%" }}
       />
     </div>
   );
