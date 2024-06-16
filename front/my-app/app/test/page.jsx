d25412134f4603973a4292f7bd7d08230a5dac0b
siosawa <salt.mail0324@gmail.com>
2024-06-17 04:16:21 +0900

    chore

--- front/my-app/app/test/page.jsx
+++ front/my-app/app/test/page.jsx
@@ -1,6 +1,19 @@
-import "../api/dashboards";
-const page = () => {
-  return <div>page</div>;
+import Image from "next/image";
+
+const AvatarComponent = () => {
+  const avatarUrl = "http://localhost:3000/uploads/user/avatar/2/IMG_1515.jpg";
+
+  return (
+    <div>
+      <p className="text-3xl mt-16">アバターを表示</p>
+      <Image
+        src={avatarUrl}
+        alt="User Avatar"
+        width={500} // 必要に応じてサイズを指定
+        height={150} // 必要に応じてサイズを指定
+      />
+    </div>
+  );
 };
 
-export default page;
+export default AvatarComponent;
