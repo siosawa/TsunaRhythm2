6e92c0a37c3a54cb0368b9ba0941c875956dc5ad
siosawa <salt.mail0324@gmail.com>
2024-06-18 21:44:32 +0900

    fix:projectsテーブルのFKを正しく再設定し依存関係やバリデーションを強化

--- back/db/migrate/20240617172140_create_projects.rb
+++ back/db/migrate/20240617172140_create_projects.rb
@@ -3,7 +3,7 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-      t.integer :user_id
+      t.references :user, null: false, foreign_key: true
       t.string :company
       t.string :name
       t.string :work_type
