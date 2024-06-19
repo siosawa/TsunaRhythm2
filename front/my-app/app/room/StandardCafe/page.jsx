1aa63cdfe72463a28f03c9df9072fa19a706c163
siosawa <salt.mail0324@gmail.com>
2024-06-19 10:38:37 +0900

    chore

--- front/my-app/app/room/StandardCafe/page.jsx
+++ front/my-app/app/room/StandardCafe/page.jsx
@@ -13,6 +13,43 @@ export default function Timer() {
   const [timerRecords, setTimerRecords] = useState([]);
   const [projects, setProjects] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
+  const [userAvatars, setUserAvatars] = useState([]);
+
+  useEffect(() => {
+    const fetchRoomMembers = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/roomMembers");
+        const roomMembers = await response.json();
+        const filteredUserIds = roomMembers
+          .filter(
+            (member) =>
+              member.room_id === 1 &&
+              new Date(member.entered_at) < new Date() &&
+              member.leaved_at === null
+          )
+          .map((member) => member.user_id);
+
+        const userPromises = filteredUserIds.map(async (userId) => {
+          const userResponse = await fetch(
+            `http://localhost:3000/api/v1/users/${userId}`
+          );
+          return userResponse.json();
+        });
+
+        const users = await Promise.all(userPromises);
+        const avatars = users.map((user) => ({
+          id: user.id,
+          avatarUrl: user.avatar.url,
+          name: user.name,
+        }));
+        setUserAvatars(avatars);
+      } catch (error) {
+        console.error("Error fetching room members or user data:", error);
+      }
+    };
+
+    fetchRoomMembers();
+  }, []);
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -59,6 +96,31 @@ export default function Timer() {
       </div>
       <GroupChat className="absolute z-30" />
       <RoomExitButton />
+      <div
+        className="absolute right-44 flex -space-x-4 z-30 p-4"
+        style={{ top: "185mm" }}
+      >
+        {userAvatars.map((user) =>
+          user.avatarUrl ? (
+            <img
+              key={user.id}
+              src={`http://localhost:3000${user.avatarUrl}`}
+              alt={user.name}
+              width={80}
+              height={80}
+              className="rounded-full"
+            />
+          ) : (
+            <div
+              key={user.id}
+              className="flex items-center justify-center bg-gray-300 text-white text-xs font-bold rounded-full"
+              style={{ width: 80, height: 80, whiteSpace: "nowrap" }}
+            >
+              NO IMAGE
+            </div>
+          )
+        )}
+      </div>
     </>
   );
 }
