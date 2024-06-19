acad19e3f3eb504d514cf072bc71360fa5e1c0e9
siosawa <salt.mail0324@gmail.com>
2024-06-19 14:59:58 +0900

    fix: RecordsテーブルのCRUD機能を追加

---
+++ front/my-app/app/room/components/EditRecord.jsx
@@ -0,0 +1,234 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import { v4 as uuidv4 } from "uuid";
+import { CiCirclePlus } from "react-icons/ci";
+import { RiDeleteBinLine } from "react-icons/ri";
+
+const EditRecord = ({ onClose, onSave }) => {
+  const [data, setData] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
+  const [projects, setProjects] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [recordsResponse, projectsResponse] = await Promise.all([
+          axios.get(`http://localhost:3000/api/v1/records`, {
+            withCredentials: true,
+          }),
+          axios.get(`http://localhost:3000/api/v1/projects`, {
+            withCredentials: true,
+          }),
+        ]);
+
+        const projectsMap = projectsResponse.data.reduce((acc, project) => {
+          acc[project.id] = project.name;
+          return acc;
+        }, {});
+
+        const dataWithId = recordsResponse.data.map((item) => ({
+          ...item,
+          id: uuidv4(),
+          originalId: item.id,
+        }));
+
+        setData(dataWithId);
+        setOriginalData(dataWithId);
+        setProjects(projectsMap);
+      } catch (error) {
+        console.error("データの取得に失敗しました", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleChange = (e, id, field) => {
+    const newData = data.map((item) => {
+      if (item.id === id) {
+        let value = e.target.value;
+        if (e.target.type === "checkbox") {
+          value = e.target.checked;
+        }
+        return { ...item, [field]: value };
+      }
+      return item;
+    });
+    setData(newData);
+  };
+
+  const handleNumberChange = (e, id, field) => {
+    const value = Math.max(0, e.target.value);
+    handleChange({ target: { value } }, id, field);
+  };
+
+  const addEmptyRow = () => {
+    const newRow = {
+      id: uuidv4(),
+      project_id: "",
+      minutes: 0,
+      date: new Date().toISOString(),
+    };
+    setData([newRow, ...data]);
+  };
+
+  const deleteRow = (id) => {
+    const newData = data.filter((item) => item.id !== id);
+    setData(newData);
+  };
+
+  const saveData = async () => {
+    const originalIds = originalData.map((item) => item.id);
+    const currentIds = data.map((item) => item.id);
+
+    const idsToDelete = originalIds.filter((id) => !currentIds.includes(id));
+    const idsToAdd = currentIds.filter((id) => !originalIds.includes(id));
+    const idsToUpdate = currentIds.filter((id) => originalIds.includes(id));
+
+    for (const id of idsToDelete) {
+      const originalItem = originalData.find((item) => item.id === id);
+      if (originalItem) {
+        try {
+          await axios.delete(
+            `http://localhost:3000/api/v1/records/${originalItem.originalId}`,
+            {
+              withCredentials: true,
+            }
+          );
+        } catch (error) {
+          console.error("Error deleting data:", error);
+        }
+      }
+    }
+
+    for (const id of idsToAdd) {
+      const newItem = data.find((item) => item.id === id);
+      if (newItem) {
+        try {
+          await axios.post(`http://localhost:3000/api/v1/records`, newItem, {
+            withCredentials: true,
+          });
+        } catch (error) {
+          console.error("Error adding data:", error);
+        }
+      }
+    }
+
+    for (const id of idsToUpdate) {
+      const originalItem = originalData.find((item) => item.id === id);
+      const newItem = data.find((item) => item.id === id);
+      if (
+        originalItem &&
+        newItem &&
+        JSON.stringify(originalItem) !== JSON.stringify(newItem)
+      ) {
+        try {
+          await axios.put(
+            `http://localhost:3000/api/v1/records/${originalItem.originalId}`,
+            newItem,
+            {
+              withCredentials: true,
+            }
+          );
+        } catch (error) {
+          console.error("Error updating data:", error);
+        }
+      }
+    }
+
+    onSave();
+  };
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-start z-50">
+      <div className="mt-16">
+        <div className="bg-white rounded-3xl shadow-custom-dark max-w-3xl w-full px-6 pb-6 pt-10 relative mx-7">
+          <Button
+            onClick={onClose}
+            className="absolute top-4 right-4 bg-gray-300 hover:bg-gray-400 text-black"
+          >
+            ✕
+          </Button>
+          <div className="flex items-center mb-4">
+            <Button
+              onClick={saveData}
+              className="bg-emerald-500 hover:bg-emerald-600"
+            >
+              保存
+            </Button>
+            <button onClick={addEmptyRow}>
+              <CiCirclePlus className="text-5xl m-2" />
+            </button>
+          </div>
+          <div className="overflow-y-auto max-h-[650px]">
+            <table className="min-w-full table-auto border-collapse border border-gray-200">
+              <thead className="bg-gray-100 whitespace-nowrap">
+                <tr>
+                  <th className="px-2 py-1 border border-gray-200">
+                    プロジェクト
+                  </th>
+                  <th className="px-2 py-1 border border-gray-200">分数</th>
+                  <th className="px-2 py-1 border border-gray-200">日付</th>
+                  <th className="px-2 py-1 border border-gray-200 w-1/12">
+                    操作
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item) => (
+                  <tr key={item.id} className="text-center">
+                    <td className="px-1 py-1 border border-gray-200">
+                      <select
+                        value={item.project_id}
+                        onChange={(e) => handleChange(e, item.id, "project_id")}
+                        className="w-full px-1 py-1"
+                      >
+                        <option value="">選択してください</option>
+                        {Object.keys(projects).map((projectId) => (
+                          <option key={projectId} value={projectId}>
+                            {projects[projectId]}
+                          </option>
+                        ))}
+                      </select>
+                    </td>
+                    <td className="px-1 py-1 border border-gray-200">
+                      <input
+                        type="number"
+                        value={item.minutes}
+                        onChange={(e) =>
+                          handleNumberChange(e, item.id, "minutes")
+                        }
+                        className="w-full px-1 py-1"
+                        min="0"
+                      />
+                    </td>
+                    <td className="px-1 py-1 border border-gray-200">
+                      <input
+                        type="datetime-local"
+                        value={new Date(item.date).toISOString().slice(0, 16)}
+                        onChange={(e) => handleChange(e, item.id, "date")}
+                        className="w-full px-1 py-1"
+                      />
+                    </td>
+                    <td className="px-1 py-1 border border-gray-200">
+                      <Button
+                        onClick={() => deleteRow(item.id)}
+                        className="bg-white text-brack hover:bg-gray-50"
+                      >
+                        <RiDeleteBinLine />
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditRecord;
--- front/my-app/app/room/components/ViewTimerRecord.jsx
+++ front/my-app/app/room/components/ViewTimerRecord.jsx
@@ -1,153 +1,142 @@
 "use client";
-import React, { useState } from "react";
-import { EditTimer } from "./EditTimer";
-import { TbTriangleInvertedFilled } from "react-icons/tb";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import EditRecord from "./EditRecord"; // EditRecordコンポーネントをインポート
 
-export function ViewTimerRecord({ timerRecords, setTimerRecords, projects }) {
-  const [isEditing, setIsEditing] = useState(false);
-  const [editRecords, setEditRecords] = useState([]);
-  const [isHidden, setIsHidden] = useState(false);
+const ViewTimerRecord = () => {
+  const [records, setRecords] = useState([]);
+  const [projects, setProjects] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [isEditOpen, setIsEditOpen] = useState(false);
 
-  const handleEdit = (index, field, value) => {
-    const updatedRecords = [...editRecords];
-    updatedRecords[index][field] = value;
-    setEditRecords(updatedRecords);
+  useEffect(() => {
+    const fetchRecords = async () => {
+      try {
+        const [recordsResponse, projectsResponse] = await Promise.all([
+          axios.get("http://localhost:3000/api/v1/records", {
+            withCredentials: true,
+          }),
+          axios.get("http://localhost:3000/api/v1/projects", {
+            withCredentials: true,
+          }),
+        ]);
+
+        const projectsMap = projectsResponse.data.reduce((acc, project) => {
+          acc[project.id] = project.name;
+          return acc;
+        }, {});
+
+        setRecords(recordsResponse.data);
+        setProjects(projectsMap);
+      } catch (error) {
+        console.error("データの取得に失敗しました", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchRecords();
+  }, []);
+
+  const fetchRecords = async () => {
+    try {
+      const [recordsResponse, projectsResponse] = await Promise.all([
+        axios.get("http://localhost:3000/api/v1/records", {
+          withCredentials: true,
+        }),
+        axios.get("http://localhost:3000/api/v1/projects", {
+          withCredentials: true,
+        }),
+      ]);
+
+      const projectsMap = projectsResponse.data.reduce((acc, project) => {
+        acc[project.id] = project.name;
+        return acc;
+      }, {});
+
+      setRecords(recordsResponse.data);
+      setProjects(projectsMap);
+    } catch (error) {
+      console.error("データの取得に失敗しました", error);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  const handleDateChange = (index, value) => {
-    const date = new Date(value);
-    handleEdit(index, "date", date);
-  };
+  if (loading) {
+    return <p>読み込み中...</p>;
+  }
 
-  const handleTimeChange = (index, field, value) => {
-    const number = parseInt(value, 10);
-    handleEdit(index, field, number);
-  };
-
-  const handleSave = () => {
-    setTimerRecords(editRecords);
-    setIsEditing(false);
-  };
-
-  const handleClose = () => {
-    setEditRecords(timerRecords);
-    setIsEditing(false);
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return `${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${String(
+      date.getMinutes()
+    ).padStart(2, "0")}`;
   };
 
   const handleEditClick = () => {
-    setEditRecords([...timerRecords]);
-    setIsEditing(true);
+    setIsEditOpen(true); // 編集モーダルを表示する
   };
 
-  const formatDate = (date) => {
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    return `${month}/${day} ${hours}:${minutes.toString().padStart(2, "0")}`;
+  const handleClose = () => {
+    setIsEditOpen(false); // 編集モーダルを閉じる
   };
 
-  const handleToggle = () => {
-    setIsHidden(!isHidden);
+  const handleSave = async () => {
+    setLoading(true);
+    await fetchRecords(); // データを再フェッチして最新の状態に更新
+    setIsEditOpen(false);
   };
 
   return (
-    <>
-      <div className="w-44">
-        <div className="bg-gray-200 rounded-3xl p-3 max-h-96 overflow-y-auto relative">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead>
-              <tr>
-                <th colSpan="3" className="text-right px-2">
-                  <div className="flex items-center justify-end">
-                    <p className="flex-grow text-center font-light">作業記録</p>
-                    <TbTriangleInvertedFilled
-                      className="inline-block cursor-pointer ml-2"
-                      onClick={handleToggle}
-                    />
-                  </div>
-                </th>
+    <div>
+      <div className="bg-white rounded-3xl shadow-custom-dark h-60 max-w-2xl overflow-y-auto w-44">
+        <table className="min-w-full table-auto border-collapse border border-gray-200 text-left text-xs">
+          <thead className="bg-gray-100 sticky top-0 z-10 text-center">
+            <tr>
+              <th className="px-1 py-1 border border-gray-200 whitespace-nowrap">
+                日付
+              </th>
+              <th className="px-1 py-1 border border-gray-200 whitespace-nowrap">
+                分数
+              </th>
+              <th className="px-1 py-1 border border-gray-200 whitespace-nowrap">
+                案件名
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.map((record) => (
+              <tr key={record.id}>
+                <td className="px-1 py-1 border border-gray-200">
+                  {formatDate(record.date)}
+                </td>
+                <td className="px-1 py-1 border border-gray-200">
+                  {record.minutes}
+                </td>
+                <td className="px-1 py-1 border border-gray-200">
+                  {projects[record.project_id] || "不明"}
+                </td>
               </tr>
-              {!isHidden && timerRecords.length > 0 && (
-                <tr>
-                  <th className="w-1/4 text-xs text-left whitespace-nowrap">
-                    日付
-                  </th>
-                  <th className="w-1/6 text-xs text-left whitespace-nowrap">
-                    分
-                  </th>
-                  <th className="w-1/5 text-xs text-left whitespace-nowrap">
-                    案件名
-                  </th>
-                </tr>
-              )}
-            </thead>
-            {!isHidden && timerRecords.length > 0 && (
-              <tbody>
-                {timerRecords.map((record, index) => (
-                  <tr key={index}>
-                    <td className="w-1/3 p-0 text-left whitespace-nowrap">
-                      <input
-                        type="text"
-                        value={formatDate(record.date)}
-                        readOnly
-                        className="border p-0 w-full text-xs"
-                      />
-                    </td>
-                    <td className="w-1/5 p-0 text-left whitespace-nowrap">
-                      <input
-                        type="number"
-                        value={record.minutes}
-                        min="0"
-                        readOnly
-                        className="border p-0 w-full text-xs"
-                      />
-                    </td>
-                    <td className="w-1/6 p-0 whitespace-nowrap">
-                      <select
-                        value={record.project}
-                        disabled
-                        className="border p-0 w-full text-xs"
-                      >
-                        {projects.map((project, i) => (
-                          <option key={i} value={project}>
-                            {project}
-                          </option>
-                        ))}
-                      </select>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            )}
-          </table>
-          {!isHidden && timerRecords.length === 0 && (
-            <div className="text-center text-xs py-2">作業記録がありません</div>
-          )}
-        </div>
-
-        {!isHidden && timerRecords.length > 0 && (
-          <div className="flex justify-end">
-            <button
-              onClick={handleEditClick}
-              className="bg-white shadow-custom-dark rounded-md mt-2 mr-3 px-2 py-1 text-xs whitespace-nowrap hover:bg-gray-200"
-            >
-              編集
-            </button>
-          </div>
-        )}
-        {isEditing && (
-          <EditTimer
-            editRecords={editRecords}
-            handleDateChange={handleDateChange}
-            handleTimeChange={handleTimeChange}
-            handleEdit={handleEdit}
-            handleSave={handleSave}
-            handleClose={handleClose}
-            projects={projects}
-          />
-        )}
+            ))}
+          </tbody>
+          <tfoot className="bg-gray-100 sticky bottom-0 z-10">
+            <tr>
+              <td
+                colSpan="3"
+                className="px-1 py-1 border border-gray-200 text-center"
+              >
+                <button className="hover:underline" onClick={handleEditClick}>
+                  編集
+                </button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
-    </>
+      {isEditOpen && <EditRecord onClose={handleClose} onSave={handleSave} />}
+    </div>
   );
-}
+};
+
+export default ViewTimerRecord;
