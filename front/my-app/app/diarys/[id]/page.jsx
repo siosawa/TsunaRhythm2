8b90389f6faf081afe770fca2b50d2d865b52ee6
siosawa <salt.mail0324@gmail.com>
2024-06-20 17:49:55 +0900

    add:アイコンを表示

--- front/my-app/app/diarys/[id]/page.jsx
+++ front/my-app/app/diarys/[id]/page.jsx
@@ -18,7 +18,7 @@ const PostDetail = () => {
           `http://localhost:3000/api/v1/posts/${id}`,
           {
             withCredentials: true,
-          },
+          }
         );
         setPost(postRes.data);
         const userId = postRes.data.user_id;
@@ -28,7 +28,7 @@ const PostDetail = () => {
           `http://localhost:3000/api/v1/users/${userId}`,
           {
             withCredentials: true,
-          },
+          }
         );
         setUser(userRes.data);
       } catch (error) {
@@ -55,7 +55,22 @@ const PostDetail = () => {
   return (
     <div className="max-w-2xl mx-auto p-4 bg-white rounded-3xl shadow-custom-dark mt-12">
       <div className="flex items-center mb-4">
-        <div className="w-10 h-10 bg-gray-200 rounded-full mr-4 flex-shrink-0"></div>
+        {user.avatar.url ? (
+          <img
+            src={`http://localhost:3000${user.avatar.url}`}
+            alt={user.name}
+            width={60}
+            height={60}
+            className="rounded-full mr-4"
+          />
+        ) : (
+          <div
+            className="flex items-center justify-center bg-gray-300 text-white text-xs font-bold rounded-full mr-4"
+            style={{ width: 65, height: 65, whiteSpace: "nowrap" }}
+          >
+            NO IMAGE
+          </div>
+        )}
         <div>
           <p className="text-lg font-semibold">{user.name}</p>
           <p className="text-sm text-gray-500">{formattedDate}</p>
