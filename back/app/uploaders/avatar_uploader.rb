b14b1fd4a745f3e604db710a12d8618553680aec
siosawa <salt.mail0324@gmail.com>
2024-06-25 15:37:12 +0900

    add:アカウント作成時に自動で画像がランダムで割り当てられるように設定

--- back/app/uploaders/avatar_uploader.rb
+++ back/app/uploaders/avatar_uploader.rb
@@ -2,12 +2,20 @@
 
 class AvatarUploader < CarrierWave::Uploader::Base
   storage :file
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # 受け付け可能なファイルの拡張子を指定
   def extension_allowlist
-    %w[jpg jpeg png]
+    %w[jpg jpeg png webp]
+  end
+
+  # デフォルトのURLを指定
+  def default_url(*args)
+    random_number = rand(1..25)
+    "/uploads/user/sample_avatar/#{random_number}.webp"
   end
 end
+
