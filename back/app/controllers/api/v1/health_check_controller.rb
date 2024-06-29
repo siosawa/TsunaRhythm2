5f0d09365805c08e6f963ae40926b1a066144b06
siosawa <salt.mail0324@gmail.com>
2024-06-29 18:37:36 +0900

    refactor: rubocop -a,-A

--- back/app/controllers/api/v1/health_check_controller.rb
+++ back/app/controllers/api/v1/health_check_controller.rb
@@ -1,5 +1,11 @@
-class Api::V1::HealthCheckController < Api::V1::ApplicationController
-    def index
-      render json: { message: "Success Health Check!!!" }, status: :ok
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class HealthCheckController < Api::V1::ApplicationController
+      def index
+        render json: { message: 'Success Health Check!!!' }, status: :ok
+      end
     end
+  end
 end
