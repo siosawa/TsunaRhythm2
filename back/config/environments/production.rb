a80b71caaac90c50f6a65b683a6f9d93775c85e6
siosawa <salt.mail0324@gmail.com>
2024-07-02 18:08:22 +0900

    add:リアルタイム機能を本番環境でも使えるように設定(失敗)

--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -3,13 +3,13 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  #httpsなら config.action_cable.url = 'ws://localhost:3000/cable' 
-  # 本番環境のWebSocket URLを設定
-  config.action_cable.url = 'wss://tsunarhythm.com/cable'
-  config.action_cable.allowed_request_origins = ['https://tsunarhythm.com']
+
+  # Action Cable設定
+  config.action_cable.url = "wss://tsunathythm.com/cable"
+  config.action_cable.allowed_request_origins = [ 'https://tsunathythm.com', /https:\/\/tsunathythm.*/ ]
+  ActionCable.server.config.disable_request_forgery_protection = true
 
   config.cache_classes = true
-
   config.eager_load = true
 
   config.consider_all_requests_local = false
@@ -19,10 +19,9 @@ Rails.application.configure do
 
   config.assets.compile = false
 
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   config.log_level = :info
-
   config.log_tags = [:request_id]
 
   config.action_mailer.perform_caching = false
@@ -30,9 +29,7 @@ Rails.application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
-
   config.active_support.disallowed_deprecation = :log
-
   config.active_support.disallowed_deprecation_warnings = []
 
   config.log_formatter = Logger::Formatter.new
@@ -45,8 +42,7 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.active_storage.service = :amazon
-
+  # セッションの設定
   config.middleware.use ActionDispatch::Session::CookieStore,
                         domain: :all,
                         tld_length: 2,
