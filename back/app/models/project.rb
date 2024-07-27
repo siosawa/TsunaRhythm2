c84d0b318565557694581a52a12bb6a70f95b818
siosawa <salt.mail0324@gmail.com>
2024-07-27 17:22:53 +0900

    fix: rubocop -a, projectのバリデーションで使うlocalesを設定

--- back/app/models/project.rb
+++ back/app/models/project.rb
@@ -4,11 +4,10 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :records, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: { scope: :user_id, message: "案件名は重複しないようにしてください" }
+  validates :name, presence: true, uniqueness: { scope: :user_id, message: :taken_with_user }
   validates :company, presence: true
   validates :work_type, presence: true
   validates :unit_price, numericality: { only_integer: true }
   validates :quantity, numericality: { only_integer: true }
   validates :is_completed, inclusion: { in: [true, false] }
 end
-
