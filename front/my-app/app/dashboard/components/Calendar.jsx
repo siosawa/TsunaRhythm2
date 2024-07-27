4ceeff2b1a0de09520543b7fb527af59e8d08187
siosawa <salt.mail0324@gmail.com>
2024-07-27 17:08:06 +0900

    不要なファイルを削除

--- front/my-app/app/dashboard/components/Calendar.jsx
+++
@@ -1,8 +0,0 @@
-// カレンダー
-"use client";
-import { Calendar } from "@/components/ui/calendar";
-export function CalendarWhite() {
-  const [date, setDate] = (React.useState < Date) | (undefined > new Date());
-
-  return <Calendar mode="single" selected={date} onSelect={setDate} />;
-}
