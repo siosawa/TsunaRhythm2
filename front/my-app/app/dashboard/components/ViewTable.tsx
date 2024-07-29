5c0c335a2dda69bd31f55f24ac515fc5669bf011
siosawa <salt.mail0324@gmail.com>
2024-07-30 01:42:03 +0900

    fix: TypeScript化、UIの改善

--- front/my-app/app/dashboard/components/ViewTable.tsx
+++ front/my-app/app/dashboard/components/ViewTable.tsx
@@ -3,26 +3,13 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import EditTable from "./EditTable"; // EditTableコンポーネントのパスを適切に設定してください
 
-interface Project {
-  id: number;
-  user_id: number;
-  company: string;
-  name: string;
-  work_type: string;
-  unit_price: number;
-  quantity: number;
-  is_completed: boolean;
-  created_at: string;
-  updated_at: string;
-}
-
 const ViewTable = () => {
-  const [projects, setProjects] = useState<Project[]>([]);
-  const [showEditTable, setShowEditTable] = useState<boolean>(false);
+  const [projects, setProjects] = useState([]);
+  const [showEditTable, setShowEditTable] = useState(false);
 
   const fetchProjects = async () => {
     try {
-      const response = await axios.get<Project[]>(
+      const response = await axios.get(
         `${process.env.NEXT_PUBLIC_API_BASE_URL}/projects`,
         {
           withCredentials: true,
@@ -45,54 +32,32 @@ const ViewTable = () => {
 
   return (
     <div>
-      <div className="bg-white rounded-3xl shadow-custom-dark h-60 max-w-2xl overflow-y-auto">
+      <div className="mt-6 w-96 h-56 bg-white rounded-3xl shadow-custom-dark max-w-2xl overflow-y-auto">
         <table className="min-w-full table-auto border-collapse border border-gray-200">
           <thead className="bg-gray-100 sticky top-0 z-10">
             <tr>
-              <th className="px-2 py-1 border border-gray-200">企業名</th>
-              <th className="px-2 py-1 border border-gray-200">案件名</th>
-              <th className="px-2 py-1 border border-gray-200 w-32">
-                ワークの種類
-              </th>
-              <th className="px-2 py-1 border border-gray-200">単価</th>
-              <th className="px-2 py-1 border border-gray-200 w-14">本数</th>
-              <th className="px-2 py-1 border border-gray-200 w-14">状態</th>
+              <th className="px-2 py-1 text-sm border border-gray-200 w-24">案件名</th>
+              <th className="px-1 py-1 text-sm border border-gray-200 w-32">ワークの種類</th>
+              <th className="px-1 py-1 text-sm border border-gray-200">単価</th>
+              <th className="px-2 py-1 text-sm border border-gray-200 w-14">本数</th>
+              <th className="px-2 py-1 text-sm border border-gray-200 w-14">状態</th>
             </tr>
           </thead>
           <tbody>
             {projects.map((project) => (
               <tr key={project.id} className="text-center">
-                <td className="px-1 py-1 border border-gray-200">
-                  {project.company}
-                </td>
-                <td className="px-1 py-1 border border-gray-200">
-                  {project.name}
-                </td>
-                <td className="px-1 py-1 border border-gray-200">
-                  {project.work_type}
-                </td>
-                <td className="px-1 py-1 border border-gray-200">
-                  {project.unit_price}
-                </td>
-                <td className="px-1 py-1 border border-gray-200">
-                  {project.quantity}
-                </td>
-                <td className="px-1 py-1 border border-gray-200">
-                  {project.is_completed ? "完" : "未"}
-                </td>
+                <td className="px-1 py-1 border border-gray-200 text-sm">{project.name}</td>
+                <td className="px-1 py-1 border border-gray-200 text-sm">{project.work_type}</td>
+                <td className="px-1 py-1 border border-gray-200 text-sm">{project.unit_price}</td>
+                <td className="px-1 py-1 border border-gray-200 text-sm">{project.quantity}</td>
+                <td className="px-1 py-1 border border-gray-200 text-sm">{project.is_completed ? "完" : "未"}</td>
               </tr>
             ))}
           </tbody>
           <tfoot className="bg-gray-100 sticky bottom-0 z-10">
             <tr>
-              <td
-                colSpan={6}
-                className="px-2 py-1 border border-gray-200 text-center"
-              >
-                <button
-                  className="hover:underline"
-                  onClick={() => setShowEditTable(true)}
-                >
+              <td colSpan={5} className="px-2 py-1 border border-gray-200 text-center">
+                <button className="hover:underline" onClick={() => setShowEditTable(true)}>
                   編集
                 </button>
               </td>
@@ -101,10 +66,7 @@ const ViewTable = () => {
         </table>
       </div>
       {showEditTable && (
-        <EditTable
-          onClose={() => setShowEditTable(false)}
-          onSave={handleSave}
-        />
+        <EditTable onClose={() => setShowEditTable(false)} onSave={handleSave} />
       )}
     </div>
   );
