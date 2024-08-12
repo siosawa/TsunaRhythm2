a058808b693f514b8e93786262d4239aee6e63ff
siosawa <salt.mail0324@gmail.com>
2024-08-12 22:29:03 +0900

    fix: TypeScript化

--- front/my-app/app/users/components/FetchUsers.ts
+++ front/my-app/app/users/components/FetchUsers.ts
@@ -1,19 +1,51 @@
 import { useEffect } from "react";
 import axios from "axios";
 
+interface User {
+  id: number;
+  name: string;
+  created_at: string;
+  work: string;
+  profile_text: string;
+  avatar: {
+    url: string;
+  };
+  posts_count: number;
+  followers_count: number;
+  following_count: number;
+}
+
+interface FollowingUser extends User {
+  relationship_id: number;
+}
+
+interface FetchUsersResponse {
+  users: User[];
+  total_pages: number;
+}
+
+interface FetchFollowingResponse {
+  users: FollowingUser[];
+}
+
+interface FollowStates {
+  [key: string]: boolean | number;
+  [key: number]: boolean;
+}
+
 const FetchUsers = (
-  currentPage,
-  currentUserId,
-  setUsers,
-  setTotalPages,
-  setFollowings,
-  setFollowStates,
-  setError,
+  currentPage: number,
+  currentUserId: number | null,
+  setUsers: (users: User[]) => void,
+  setTotalPages: (totalPages: number) => void,
+  setFollowings: (followings: Set<number>) => void,
+  setFollowStates: (followStates: FollowStates) => void,
+  setError: (error: string) => void,
 ) => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const res = await axios.get(
+        const res = await axios.get<FetchUsersResponse>(
           `${process.env.NEXT_PUBLIC_API_BASE_URL}/users?page=${currentPage}`,
           {
             withCredentials: true,
@@ -36,7 +68,7 @@ const FetchUsers = (
     const fetchFollowing = async () => {
       if (currentUserId) {
         try {
-          const response = await axios.get(
+          const response = await axios.get<FetchFollowingResponse>(
             `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/${currentUserId}/following`,
             {
               withCredentials: true,
@@ -45,7 +77,7 @@ const FetchUsers = (
           const data = response.data;
 
           const followingSet = new Set(data.users.map((user) => user.id));
-          const followingStates = {};
+          const followingStates: FollowStates = {};
           data.users.forEach((user) => {
             followingStates[user.id] = true;
             followingStates[`relationship_${user.id}`] = user.relationship_id;
