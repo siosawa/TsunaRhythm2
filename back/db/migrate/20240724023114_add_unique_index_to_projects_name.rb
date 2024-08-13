0b764e28313254dfcbbb31d8cb1176b74f918610
siosawa <salt.mail0324@gmail.com>
2024-08-14 00:13:23 +0900

    fix: 本番環境でもバリデーションが解除されるように設定(remove_indexを追加)

--- back/db/migrate/20240724023114_add_unique_index_to_projects_name.rb
+++ back/db/migrate/20240724023114_add_unique_index_to_projects_name.rb
@@ -1,6 +1,5 @@
 class AddUniqueIndexToProjectsName < ActiveRecord::Migration[7.0]
   def change
-    # 案件名を一意にしたいならロールバックしてコメントアウトを外してマイグレート
-    # add_index :projects, [:user_id, :name], unique: true
+    add_index :projects, [:user_id, :name], unique: true
   end
 end
